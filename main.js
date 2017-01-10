5f41c6ca32bee23c628f9aa8355ac33bb0039998
Dragan Marjanovic <dmarj97@gmail.com>
2017-01-10 21:56:37 +1000

    Fixed date issue.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
  * Stravasicht
  *
- * A simple widget to display Strava data.
+ * A simple (but configurable) widget to display Strava data.
  *
  * Author: Dragan Marjanovic - marjanovic.io
  */
@@ -11,7 +11,12 @@
 // Set your access token here
 accessToken: "",
 
-refreshFrequency: 500, // How often to refresh the widget
+refreshFrequency: 1000, // How often to refresh the widget
+
+// Options: distance, elapsed_time, average_speed, max_speed
+metric: "distance", // The metric to track and display
+
+rate_tracker: 0,
 
 httpGet: function(url) {
     var xmlHttp = new XMLHttpRequest(); // Create the request object
@@ -20,39 +25,90 @@ httpGet: function(url) {
     return xmlHttp.responseText; //Return the response
 },
 
-
-command: function (callback) {
+getData: function () {
     var baseURL = "https://www.strava.com/api/v3/athlete/activities";
     var requestURL = baseURL + "?access_token=" + this.accessToken;
+    this.rate_tracker++;
+    return this.httpGet(requestURL);
+},
+
+getDayCount: function(now) {
+    var start = new Date(now.getFullYear(), 0, 0);
+    console.log("Start: " + start);
+    console.log("Now: " + now);
+    var diff = now - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    console.log("Frac: " + (diff/oneDay) + "Diff:" + diff + "oneDay" + oneDay);
+    return Math.floor(diff / oneDay);
+},
+
+command: function (callback) {
+    requestData = this.getData();
     // Feed the data back to the renderer for processing
-    callback(0, this.httpGet(requestURL));
+    callback(0, requestData);
 },
 
 render: function (output) {
-    // Process data
-    // Render data
+    // Initialise the Chart library and canvas container
     content = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js\"></script>"
-    // content += "<div><h6>" + output + "</h6></div>";
     content += '<canvas id="myChart"></canvas>'
-
-
     return content;
 },
 
 afterRender: function(domEl) {
     var ctx = document.getElementById('myChart');
 
-    var labz = []
-    var dats = []
-    for (var i = 0; i < 365; i++) {
-        labz.push("");
-        dats.push(Math.random() * 100);
+    activityData = JSON.parse(this.getData());
+
+
+    var trackedMetricData = [];
+    var labelData = []
+
+    var day = this.getDayCount(new Date());
+
+    for (var i = 0; i < day; i++) {
+        labelData.push("");
+        trackedMetricData.push(0.001);
+    }
+    console.log(trackedMetricData);
+
+    for (var i=0; i < activityData.length; i++) {
+        var activity = activityData[i];
+        var dayBin;
+
+        // activityDate = new Date(activity.start_date_local);
+        activityDate = new Date(activity.start_date);
+        if (activityDate.getFullYear() >= new Date().getFullYear()) {
+            var sdLocal = activity.start_date_local;
+            var date = sdLocal.substring(0, sdLocal.indexOf("T"));
+            dayBin = this.getDayCount(new Date(date)) - 1;
+            switch (this.metric) {
+                case "elapsed_time":
+                    trackedMetricData[dayBin] += activity.elapsed_time/60;
+                    break;
+                case "average_speed":
+                    averageKilometres = activity.average_speed * 60 * 60 / 1000;
+                    trackedMetricData[dayBin] += averageKilometres;
+                    break;
+                case "max_speed":
+                    maxKph = activity.max_speed * 60 * 60 / 1000;
+                    trackedMetricData[dayBin] += maxKph;
+                    case "distance":
+                default: // Default should be distance
+                    console.log("Bin: " + dayBin + "\n" + "Distance:" + activity.distance/1000);
+                    trackedMetricData[dayBin] += activity.distance/1000;
+                    break;
+            }
+        }
+
     }
 
+
+    console.log(trackedMetricData);
     var dataz = {
-        labels: labz,
+        labels: labelData,
         datasets: [{
-            data: dats,
+            data: trackedMetricData,
             backgroundColor: 'rgba(255, 255, 255, 1)',
         }]
     }
@@ -62,9 +118,7 @@ afterRender: function(domEl) {
         maintainAspectRatio: false,
         barThickness: 5,
         scales: {
-            xAxes: [{
-                display: false
-            }],
+            xAxes: [{ display: false }],
             yAxes: [{
                 display: true,
                 ticks: {
@@ -79,9 +133,7 @@ afterRender: function(domEl) {
                 }
             }]
         },
-        legend: {
-            display: false
-        }
+        legend: { display: false }
     }
 
     var myChart = new Chart(ctx, {
@@ -92,10 +144,9 @@ afterRender: function(domEl) {
     this.refreshFrequency = 100000;
 },
 
-style: "        \n\
-  bottom: .25%     \n\
-  left: 0.25%    \n\
-  height: 200px \n\
-  width: 99%    \n\
-                \n\
+style: "            \n\
+  bottom: .25%      \n\
+  left: 0.25%       \n\
+  height: 200px     \n\
+  width: 99%        \n\
 "
